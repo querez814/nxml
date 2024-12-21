4ca6638ca2ee9a56deb2ffc1e625fe6edf8ee478
querez814 <quereshyzaki@gmail.com>
2024-12-21 13:31:47 -0500

    commmittting

--- src/routes/valuation/[ticker]/+page.svelte
+++ src/routes/valuation/[ticker]/+page.svelte
@@ -7,7 +7,7 @@
 				fiscalDateEnding: entry.fiscalDateEnding || '-',
 				symbol: entry.symbol || '-',
 				evtosales: entry.evtosales || '-',
-				evtobit: entry.evtobit || '-',
+				evtoebit: entry.evtoebit || '-',
 				evtoebitda: entry.evtoebitda || '-',
 				evtonetincome: entry.evtonetincome || '-',
 				AnalystTargetPrice: entry.AnalystTargetPrice || '-',
@@ -36,9 +36,6 @@
 					<th class="px-4 py-2">EV to EBIT</th>
 					<th class="px-4 py-2">EV to EBITDA</th>
 					<th class="px-4 py-2">EV to Net Income</th>
-					<th class="px-4 py-2">Analyst Target Price</th>
-					<th class="px-4 py-2">Trailing PE</th>
-					<th class="px-4 py-2">Forward PE</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -47,12 +44,9 @@
 						<td class="px-4 py-2">{entry.fiscalDateEnding}</td>
 						<td class="px-4 py-2">{entry.symbol}</td>
 						<td class="px-4 py-2">{entry.evtosales}</td>
-						<td class="px-4 py-2">{entry.evtobit}</td>
+						<td class="px-4 py-2">{entry.evtoebit}</td>
 						<td class="px-4 py-2">{entry.evtoebitda}</td>
 						<td class="px-4 py-2">{entry.evtonetincome}</td>
-						<td class="px-4 py-2">{entry.AnalystTargetPrice}</td>
-						<td class="px-4 py-2">{entry.TrailingPE}</td>
-						<td class="px-4 py-2">{entry.ForwardPE}</td>
 					</tr>
 				{/each}
 			</tbody>
