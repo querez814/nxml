2e2c978c2cfcee8ee4c79fa2cd10793764121efb
querez814 <quereshyzaki@gmail.com>
2024-12-24 05:45:19 -0500

    fix api url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	envPrefix: 'VITE_',
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
