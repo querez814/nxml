1ae51590b1fe4188e9794b666e7ed3d9d7167be0
querez814 <quereshyzaki@gmail.com>
2025-01-06 16:15:38 -0500

    deleting nonsense folder probably would be a vulnerability

--- src/routes/scratch/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import type { PageData } from './$types';
-	import Tutorial from '$lib/components/tutorial/Tutorial.svelte';
-	let { data }: { data: PageData } = $props();
-</script>
-
-<div></div>
