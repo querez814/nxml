e6f91e121c03b2a0bae6412aefc399ce0334aace
querez814 <quereshyzaki@gmail.com>
2025-01-07 09:44:08 -0500

    cleaning the api - now creating branch to handle final front end changes

--- src/api/balancesheet/balancesheetdata.ts
+++ src/api/balancesheet/balancesheetdata.ts
@@ -9,16 +9,6 @@ export const fetchBalanceSheet = async (ticker: string): Promise<any[]> => {
 	const rawData = await response.json();
 	return cleanBalanceSheetData(rawData);
 };
-export const fetchBalanceSheetRatios = async (ticker: string): Promise<any[]> => {
-	const response = await fetch(
-		`${api_url}/financials/balancesheet-statement/quarterly/${ticker}/ratios`
-	);
-	if (!response.ok) {
-		throw new Error(`Failed to fetch balance sheet ratios data: ${response.statusText}`);
-	}
-	const rawData = await response.json();
-	return cleanBalanceSheetRatiosData(rawData);
-};
 
 const cleanBalanceSheetData = (data: any[]): any[] => {
 	return data.map((item) => ({
@@ -36,18 +26,54 @@ const cleanBalanceSheetData = (data: any[]): any[] => {
 		inventory: (parseFloat(item.inventory) / 1e6).toLocaleString(),
 		propertyPlantEquipment: (parseFloat(item.propertyPlantEquipment) / 1e6).toLocaleString(),
 		deferredRevenue: (parseFloat(item.deferredRevenue) / 1e6).toLocaleString(),
-		currentDebt: (parseFloat(item.currentDebt) / 1e6).toLocaleString()
-	}));
-};
+		currentDebt: (parseFloat(item.currentDebt) / 1e6).toLocaleString(),
 
-const cleanBalanceSheetRatiosData = (data: any[]): any[] => {
-	return data.map((item) => ({
-		fiscalDateEnding: item.fiscalDateEnding,
-		current_ratio: item.current_ratio,
-		quick_ratio: item.quick_ratio,
-		cash_ratio: item.cash_ratio,
-		debt_to_equity_ratio: item.debt_to_equity_ratio,
-		debt_to_asset_ratio: item.debt_to_asset_ratio,
-		book_value_per_share: item.book_value_per_share
+		// Parsing the Balance Sheet Ratios for Display
+		current_ratio: parseFloat(item.current_ratio).toFixed(2),
+		quick_ratio: parseFloat(item.quick_ratio).toFixed(2),
+		cash_ratio: parseFloat(item.cash_ratio).toFixed(2),
+		debt_to_equity_ratio: parseFloat(item.debt_to_equity_ratio).toFixed(2),
+		debt_to_asset_ratio: parseFloat(item.debt_to_asset_ratio).toFixed(2),
+		book_value_per_share: parseFloat(item.book_value_per_share).toFixed(2),
+
+		// Year-over-Year Changes
+		totalCurrentAssets_YoY: item.totalCurrentAssets_YoY,
+		totalAssets_YoY: item.totalAssets_YoY,
+		totalCurrentLiabilities_YoY: item.totalCurrentLiabilities_YoY,
+		totalLiabilities_YoY: item.totalLiabilities_YoY,
+		working_capital_YoY: item.working_capital_YoY,
+		totalShareholderEquity_YoY: item.totalShareholderEquity_YoY,
+		commonStockSharesOutstanding_YoY: item.commonStockSharesOutstanding_YoY,
+		cashAndCashEquivalentsAtCarryingValue_YoY: item.cashAndCashEquivalentsAtCarryingValue_YoY,
+		inventory_YoY: item.inventory_YoY,
+		propertyPlantEquipment_YoY: item.propertyPlantEquipment_YoY,
+		deferredRevenue_YoY: item.deferredRevenue_YoY,
+		currentDebt_YoY: item.currentDebt_YoY,
+		current_ratio_YoY: item.current_ratio_YoY,
+		quick_ratio_YoY: item.quick_ratio_YoY,
+		cash_ratio_YoY: item.cash_ratio_YoY,
+		debt_to_equity_ratio_YoY: item.debt_to_equity_ratio_YoY,
+		debt_to_asset_ratio_YoY: item.debt_to_asset_ratio_YoY,
+		book_value_per_share_YoY: item.book_value_per_share_YoY,
+
+		// Quarter-over-Quarter Changes
+		totalCurrentAssets_QoQ: item.totalCurrentAssets_QoQ,
+		totalAssets_QoQ: item.totalAssets_QoQ,
+		totalCurrentLiabilities_QoQ: item.totalCurrentLiabilities_QoQ,
+		totalLiabilities_QoQ: item.totalLiabilities_QoQ,
+		working_capital_QoQ: item.working_capital_QoQ,
+		totalShareholderEquity_QoQ: item.totalShareholderEquity_QoQ,
+		commonStockSharesOutstanding_QoQ: item.commonStockSharesOutstanding_QoQ,
+		cashAndCashEquivalentsAtCarryingValue_QoQ: item.cashAndCashEquivalentsAtCarryingValue_QoQ,
+		inventory_QoQ: item.inventory_QoQ,
+		propertyPlantEquipment_QoQ: item.propertyPlantEquipment_QoQ,
+		deferredRevenue_QoQ: item.deferredRevenue_QoQ,
+		currentDebt_QoQ: item.currentDebt_QoQ,
+		current_ratio_QoQ: item.current_ratio_QoQ,
+		quick_ratio_QoQ: item.quick_ratio_QoQ,
+		cash_ratio_QoQ: item.cash_ratio_QoQ,
+		debt_to_equity_ratio_QoQ: item.debt_to_equity_ratio_QoQ,
+		debt_to_asset_ratio_QoQ: item.debt_to_asset_ratio_QoQ,
+		book_value_per_share_QoQ: item.book_value_per_share_QoQ
 	}));
 };
--- src/api/cashflowsheet/cashflowdata.ts
+++ src/api/cashflowsheet/cashflowdata.ts
@@ -3,87 +3,129 @@ const api_url = import.meta.env.VITE_API_URL;
 export const fetchCashFlow = async (ticker: string): Promise<any[]> => {
 	const response = await fetch(`${api_url}/financials/cashflow-statement/quarterly/${ticker}`);
 	if (!response.ok) {
-		throw new Error(`Failed to fetch balance sheet data: ${response.statusText}`);
+		throw new Error(`Failed to fetch cash flow data: ${response.statusText}`);
 	}
 
 	const rawData = await response.json();
 	return cleanCashFlowData(rawData);
 };
 
-export const fetchCashFlowYoy = async (ticker: string): Promise<any[]> => {
-	const response = await fetch(`${api_url}/financials/cashflow-statement/quarterly/${ticker}/yoy`);
-	if (!response.ok) {
-		throw new Error(`Failed to fetch cash flow yoy data: ${response.statusText}`);
-	}
-
-	const rawData = await response.json();
-	return rawData;
-};
-
-export const fetchCashFlowQoq = async (ticker: string): Promise<any[]> => {
-	const response = await fetch(`${api_url}/financials/cashflow-statement/quarterly/${ticker}/qoq`);
-	if (!response.ok) {
-		throw new Error(`Failed to fetch cash flow qoq data: ${response.statusText}`);
-	}
-
-	const rawData = await response.json();
-	return rawData;
-};
-
-export const fetchCashFlowMargins = async (ticker: string): Promise<any[]> => {
-	const response = await fetch(
-		`${api_url}/financials/cashflow-statement/quarterly/${ticker}/margins`
-	);
-	if (!response.ok) {
-		throw new Error(`Failed to fetch cash flow margin data: ${response.statusText}`);
-	}
-
-	const rawData = await response.json();
-	return cleanCashFlowMarginsData(rawData);
-};
-
-export const fetchCashFlowRatios = async (ticker: string): Promise<any[]> => {
-	const response = await fetch(
-		`${api_url}/financials/cashflow-statement/quarterly/${ticker}/ratios`
-	);
-	if (!response.ok) {
-		throw new Error(`Failed to fetch cash flow ratios data: ${response.statusText}`);
-	}
-	const rawData = await response.json();
-	return cleanCashFlowRatiosData(rawData);
-};
-
 const cleanCashFlowData = (data: any[]): any[] => {
 	return data.map((item) => ({
 		fiscalDateEnding: item.fiscalDateEnding,
-		operatingCashflow: parseFloat(item.operatingCashflow) / 1e6,
-		capitalExpenditures: parseFloat(item.capitalExpenditures) / 1e6,
-		freeCashFlow: parseFloat(item.freeCashFlow) / 1e6,
-		changeInInventory: parseFloat(item.changeInInventory) / 1e6,
-		changeInReceivables: parseFloat(item.changeInReceivables) / 1e6,
-		cashflowFromInvestment: parseFloat(item.cashFlowFromInvestment) / 1e6,
-		cashflowFromFinancing: parseFloat(item.cashflowFromFinancing) / 1e6,
-		paymentsForRepurchaseOfCommonStock: parseFloat(item.paymentsForRepurchaseOfCommonStock) / 1e6,
-		paymentsForRepurchaseOfEquity: parseFloat(item.paymentsForRepurchaseOfEquity) / 1e6,
-		dividendPayout: parseFloat(item.dividendPayout) / 1e6
-	}));
-};
 
-const cleanCashFlowMarginsData = (data: any[]): any[] => {
-	return data.map((item) => ({
-		fiscalDateEnding: item.fiscalDateEnding,
-		net_profit_margin: parseFloat(item.net_profit_margin),
-		ocf_margin: parseFloat(item.ocf_margin),
-		fcf_margin: parseFloat(item.fcf_margin)
-	}));
-};
+		operatingCashflow: (parseFloat(item.operatingCashflow) / 1e6).toLocaleString(),
+		paymentsForOperatingActivities: (
+			parseFloat(item.paymentsForOperatingActivities) / 1e6
+		).toLocaleString(),
+		proceedsFromOperatingActivities: (
+			parseFloat(item.proceedsFromOperatingActivities) / 1e6
+		).toLocaleString(),
+		changeInOperatingLiabilities: (
+			parseFloat(item.changeInOperatingLiabilities) / 1e6
+		).toLocaleString(),
+		changeInOperatingAssets: (parseFloat(item.changeInOperatingAssets) / 1e6).toLocaleString(),
+		depreciationDepletionAndAmortization: (
+			parseFloat(item.depreciationDepletionAndAmortization) / 1e6
+		).toLocaleString(),
+		capitalExpenditures: (parseFloat(item.capitalExpenditures) / 1e6).toLocaleString(),
+		changeInReceivables: (parseFloat(item.changeInReceivables) / 1e6).toLocaleString(),
+		changeInInventory: (parseFloat(item.changeInInventory) / 1e6).toLocaleString(),
+		profitLoss: (parseFloat(item.profitLoss) / 1e6).toLocaleString(),
+		cashflowFromInvestment: (parseFloat(item.cashflowFromInvestment) / 1e6).toLocaleString(),
+		cashflowFromFinancing: (parseFloat(item.cashflowFromFinancing) / 1e6).toLocaleString(),
+		proceedsFromRepaymentsOfShortTermDebt: (
+			parseFloat(item.proceedsFromRepaymentsOfShortTermDebt) / 1e6
+		).toLocaleString(),
+		paymentsForRepurchaseOfCommonStock: (
+			parseFloat(item.paymentsForRepurchaseOfCommonStock) / 1e6
+		).toLocaleString(),
+		paymentsForRepurchaseOfEquity: (
+			parseFloat(item.paymentsForRepurchaseOfEquity) / 1e6
+		).toLocaleString(),
+		paymentsForRepurchaseOfPreferredStock: (
+			parseFloat(item.paymentsForRepurchaseOfPreferredStock) / 1e6
+		).toLocaleString(),
+		dividendPayout: (parseFloat(item.dividendPayout) / 1e6).toLocaleString(),
+		dividendPayoutCommonStock: (parseFloat(item.dividendPayoutCommonStock) / 1e6).toLocaleString(),
+		dividendPayoutPreferredStock: (
+			parseFloat(item.dividendPayoutPreferredStock) / 1e6
+		).toLocaleString(),
+		proceedsFromRepurchaseOfEquity: (
+			parseFloat(item.proceedsFromRepurchaseOfEquity) / 1e6
+		).toLocaleString(),
+		netIncome: (parseFloat(item.netIncome) / 1e6).toLocaleString(),
+		freeCashFlow: (parseFloat(item.freeCashFlow) / 1e6).toLocaleString(),
+		change_working_capital: (parseFloat(item.change_working_capital) / 1e6).toLocaleString(),
 
-const cleanCashFlowRatiosData = (data: any[]): any[] => {
-	return data.map((item) => ({
-		fiscalDateEnding: item.fiscalDateEnding,
-		roce: parseFloat(item.roce),
-		cfa_ratio: parseFloat(item.cfa_ratio),
-		capex_ratio: parseFloat(item.capex_ratio),
-		change_working_capital: parseFloat(item.change_working_capital) / 1e6
+		// Parsing Cash Flow Ratios
+		net_profit_margin: item.net_profit_margin,
+		ocf_margin: item.ocf_margin,
+		fcf_margin: item.fcf_margin,
+		roce: parseFloat(item.roce).toFixed(2),
+		cash_flow_adequacy_ratio: parseFloat(item.cash_flow_adequacy_ratio).toFixed(2),
+		capex_ratio: parseFloat(item.capex_ratio).toFixed(2),
+
+		// Mapping YoY
+		operatingCashflow_YoY: item.operatingCashflow_YoY,
+		paymentsForOperatingActivities_YoY: item.paymentsForOperatingActivities_YoY,
+		proceedsFromOperatingActivities_YoY: item.proceedsFromOperatingActivities_YoY,
+		changeInOperatingLiabilities_YoY: item.changeInOperatingLiabilities_YoY,
+		changeInOperatingAssets_YoY: item.changeInOperatingAssets_YoY,
+		depreciationDepletionAndAmortization_YoY: item.depreciationDepletionAndAmortization_YoY,
+		capitalExpenditures_YoY: item.capitalExpenditures_YoY,
+		changeInReceivables_YoY: item.changeInReceivables_YoY,
+		changeInInventory_YoY: item.changeInInventory_YoY,
+		profitLoss_YoY: item.profitLoss_YoY,
+		cashflowFromInvestment_YoY: item.cashflowFromInvestment_YoY,
+		cashflowFromFinancing_YoY: item.cashflowFromFinancing_YoY,
+		proceedsFromRepaymentsOfShortTermDebt_YoY: item.proceedsFromRepaymentsOfShortTermDebt_YoY,
+		paymentsForRepurchaseOfCommonStock_YoY: item.paymentsForRepurchaseOfCommonStock_YoY,
+		paymentsForRepurchaseOfEquity_YoY: item.paymentsForRepurchaseOfEquity_YoY,
+		paymentsForRepurchaseOfPreferredStock_YoY: item.paymentsForRepurchaseOfPreferredStock_YoY,
+		dividendPayout_YoY: item.dividendPayout_YoY,
+		dividendPayoutCommonStock_YoY: item.dividendPayoutCommonStock_YoY,
+		dividendPayoutPreferredStock_YoY: item.dividendPayoutPreferredStock_YoY,
+		proceedsFromRepurchaseOfEquity_YoY: item.proceedsFromRepurchaseOfEquity_YoY,
+		netIncome_YoY: item.netIncome_YoY,
+		freeCashFlow_YoY: item.freeCashFlow_YoY,
+		net_profit_margin_YoY: item.net_profit_margin_YoY,
+		ocf_margin_YoY: item.ocf_margin_YoY,
+		fcf_margin_YoY: item.fcf_margin_YoY,
+		roce_YoY: item.roce_YoY,
+		cash_flow_adequacy_ratio_YoY: item.cash_flow_adequacy_ratio_YoY,
+		capex_ratio_YoY: item.capex_ratio_YoY,
+		change_working_capital_YoY: item.change_working_capital_YoY,
+
+		// Mapping QoQ
+		operatingCashflow_QoQ: item.operatingCashflow_QoQ,
+		paymentsForOperatingActivities_QoQ: item.paymentsForOperatingActivities_QoQ,
+		proceedsFromOperatingActivities_QoQ: item.proceedsFromOperatingActivities_QoQ,
+		changeInOperatingLiabilities_QoQ: item.changeInOperatingLiabilities_QoQ,
+		changeInOperatingAssets_QoQ: item.changeInOperatingAssets_QoQ,
+		depreciationDepletionAndAmortization_QoQ: item.depreciationDepletionAndAmortization_QoQ,
+		capitalExpenditures_QoQ: item.capitalExpenditures_QoQ,
+		changeInReceivables_QoQ: item.changeInReceivables_QoQ,
+		changeInInventory_QoQ: item.changeInInventory_QoQ,
+		profitLoss_QoQ: item.profitLoss_QoQ,
+		cashflowFromInvestment_QoQ: item.cashflowFromInvestment_QoQ,
+		cashflowFromFinancing_QoQ: item.cashflowFromFinancing_QoQ,
+		proceedsFromRepaymentsOfShortTermDebt_QoQ: item.proceedsFromRepaymentsOfShortTermDebt_QoQ,
+		paymentsForRepurchaseOfCommonStock_QoQ: item.paymentsForRepurchaseOfCommonStock_QoQ,
+		paymentsForRepurchaseOfEquity_QoQ: item.paymentsForRepurchaseOfEquity_QoQ,
+		paymentsForRepurchaseOfPreferredStock_QoQ: item.paymentsForRepurchaseOfPreferredStock_QoQ,
+		dividendPayout_QoQ: item.dividendPayout_QoQ,
+		dividendPayoutCommonStock_QoQ: item.dividendPayoutCommonStock_QoQ,
+		dividendPayoutPreferredStock_QoQ: item.dividendPayoutPreferredStock_QoQ,
+		proceedsFromRepurchaseOfEquity_QoQ: item.proceedsFromRepurchaseOfEquity_QoQ,
+		netIncome_QoQ: item.netIncome_QoQ,
+		freeCashFlow_QoQ: item.freeCashFlow_QoQ,
+		net_profit_margin_QoQ: item.net_profit_margin_QoQ,
+		ocf_margin_QoQ: item.ocf_margin_QoQ,
+		fcf_margin_QoQ: item.fcf_margin_QoQ,
+		roce_QoQ: item.roce_QoQ,
+		cash_flow_adequacy_ratio_QoQ: item.cash_flow_adequacy_ratio_QoQ,
+		capex_ratio_QoQ: item.capex_ratio_QoQ,
+		change_working_capital_QoQ: item.change_working_capital_QoQ
 	}));
 };
