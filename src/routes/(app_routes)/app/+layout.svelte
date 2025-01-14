111ff6e05c1e573c9734ace305dc7e097cc6d441
querez814 <quereshyzaki@gmail.com>
2025-01-13 20:48:51 -0500

    fixed layout error might ne the solution that prevented render

--- src/routes/(app_routes)/app/+layout.svelte
+++ src/routes/(app_routes)/app/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import AppHeader from '$lib/components/app/app-header.svelte';
 	import '../../../app.css';
 	import SignedIn from 'clerk-sveltekit/client/SignedIn.svelte';
 	import { redirect } from '@sveltejs/kit';
@@ -13,19 +14,19 @@
 </svelte:head>
 
 <SignedIn let:user>
-    { console.log(user?.publicMetadata ) }
-    <!-- Add some check here to see if user has free trial days left or not!  -->
-    {#if (alwaysRedirect)}
-        {#await goto(siteMetaData.urls.web.pricing) }
-            <!-- This will never actually render, as the redirect will happen first -->
-        {/await}
-    {/if}
-    <div class="flex flex-col min-h-screen w-full">
-        <AppHeader />
-        <main class="flex-1 flex flex-col">
-            <slot />
-        </main>
-    </div>
+	{console.log(user?.publicMetadata)}
+	<!-- Add some check here to see if user has free trial days left or not!  -->
+	{#if alwaysRedirect}
+		{#await goto(siteMetaData.urls.web.pricing)}
+			<!-- This will never actually render, as the redirect will happen first -->
+		{/await}
+	{/if}
+	<div class="flex min-h-screen w-full flex-col">
+		<AppHeader />
+		<main class="flex flex-1 flex-col">
+			<slot />
+		</main>
+	</div>
 </SignedIn>
 
 <style>
